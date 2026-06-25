28e2dbe101341cfce34a627e8a796f37dc0d77cc
sofiaIcavino <sofiaicavino@gmail.com>
2026-06-25 18:48:16 -0400

    ajeitando logica para o cenário 4

--- conversor.py
+++ conversor.py
@@ -1,5 +1,5 @@
 def fahrenheit_para_celsius(f):
-    return round((f - 32) * 5 / 10, 2)
+    return round((f - 32) * 5 / 9, 2)
 
 def celsius_para_fahrenheit(c):
     return round((c * 9 / 5) + 32, 2)
